c92fbdcb6638e9d01e11f48c67b795e8d8bc5f5b
Hafidzzz <huzzans.hafidz@gmail.com>
2026-07-15 17:01:41 +0800

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,13 @@
    Firebase Console → Project Settings → Your apps → SDK setup
    ========================================================= */
 const firebaseConfig = {
-  apiKey: "ISI_API_KEY_ANDA",
-  authDomain: "ISI_PROJECT_ID.firebaseapp.com",
-  projectId: "ISI_PROJECT_ID",
-  storageBucket: "ISI_PROJECT_ID.appspot.com",
-  messagingSenderId: "ISI_SENDER_ID",
-  appId: "ISI_APP_ID",
+   apiKey: "AIzaSyAGPE_TGTfBYdZCJkiryEfvXWLp4Hn_fhQ",
+   authDomain: "vii-bilal-2026.firebaseapp.com",
+   projectId: "vii-bilal-2026",
+   storageBucket: "vii-bilal-2026.firebasestorage.app",
+   messagingSenderId: "319968710286",
+   appId: "1:319968710286:web:a18a0a2531ef04177083df",
+   measurementId: "G-SYJ0CKK811"
 };
 
 firebase.initializeApp(firebaseConfig);
