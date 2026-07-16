28acfc88d396e22f2931e67693fc8582dcc89466
Hafidzzz <huzzans.hafidz@gmail.com>
2026-07-16 16:18:18 +0800

    Update firebaseConfig to include databaseURL
    Added databaseURL to firebase configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,16 @@
    ISI DENGAN firebaseConfig DARI PROJECT FIREBASE ANDA
    Firebase Console → Project Settings → Your apps → SDK setup
    ========================================================= */
+
 const firebaseConfig = {
-   apiKey: "AIzaSyAGPE_TGTfBYdZCJkiryEfvXWLp4Hn_fhQ",
-   authDomain: "vii-bilal-2026.firebaseapp.com",
-   projectId: "vii-bilal-2026",
-   storageBucket: "vii-bilal-2026.firebasestorage.app",
-   messagingSenderId: "319968710286",
-   appId: "1:319968710286:web:a18a0a2531ef04177083df",
-   measurementId: "G-SYJ0CKK811"
+  apiKey: "AIzaSyAGPE_TGTfBYdZCJkiryEfvXWLp4Hn_fhQ",
+  authDomain: "vii-bilal-2026.firebaseapp.com",
+  databaseURL: "https://vii-bilal-2026-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vii-bilal-2026",
+  storageBucket: "vii-bilal-2026.firebasestorage.app",
+  messagingSenderId: "319968710286",
+  appId: "1:319968710286:web:a18a0a2531ef04177083df",
+  measurementId: "G-SYJ0CKK811"
 };
 
 firebase.initializeApp(firebaseConfig);
